f5286ab8e1bde838e14209f1538178f5b542bb7e
Wendell Baresi <wendell.oliveira@zup.com.br>
2025-01-24 11:40:31 -0300

    Implementado os atributos da enum Categoria

--- src/main/java/com/example/retornosAPI/models/Categoria.java
+++ src/main/java/com/example/retornosAPI/models/Categoria.java
@@ -1,4 +1,17 @@
 package com.example.retornosAPI.models;
 
 public enum Categoria {
+    ELETRONICO("Eletrônicos"),
+    ROUPA("Roupas"),
+    ALIMENTO("Alimento");
+
+    private final String categoriaProduto;
+
+    Categoria(String categoriaProduto){
+        this.categoriaProduto = categoriaProduto;
+    }
+
+    public String getCategoriaProduto(){
+        return categoriaProduto;
+    }
 }
