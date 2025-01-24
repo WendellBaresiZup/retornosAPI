4634ebefc27f3f699508a738ee17597cb9e90f15
Wendell Baresi <wendell.oliveira@zup.com.br>
2025-01-24 14:59:54 -0300

    Implementado no record Product os parametros e alterando de portugues para ingles

--- src/main/java/com/example/retornosAPI/models/Product.java
+++ src/main/java/com/example/retornosAPI/models/Product.java
@@ -1,4 +1,4 @@
 package com.example.retornosAPI.models;
 
-public record Product() {
+public record Product(Long id, String name,String description ,Double price, String stockQuantity, Category category) {
 }
